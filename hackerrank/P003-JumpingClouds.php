a373342aebd0677667ee7103f8c4d74f979dd4c0
sosaheri <sosaheriberto2001@gmail.com>
2020-08-12 07:05:44 -0400

    challenge #3 from HackerRank Jumping Clouds

---
+++ hackerrank/P003-JumpingClouds.php
@@ -0,0 +1,59 @@
+<?php
+
+// Emma is playing a new mobile game that starts with consecutively numbered clouds. Some of the clouds are thunderheads and others are cumulus. She can jump on any cumulus cloud having a number that is equal to the number of the current cloud plus  or . She must avoid the thunderheads. Determine the minimum number of jumps it will take Emma to jump from her starting postion to the last cloud. It is always possible to win the game.
+
+// For each game, Emma will get an array of clouds numbered  if they are safe or  if they must be avoided. For example,  indexed from . The number on each cloud is its index in the list so she must avoid the clouds at indexes  and . She could follow the following two paths:  or . The first path takes  jumps while the second takes .
+
+// Function Description
+
+// Complete the jumpingOnClouds function in the editor below. It should return the minimum number of jumps required, as an integer.
+
+// jumpingOnClouds has the following parameter(s):
+
+// c: an array of binary integers
+// Input Format
+
+// The first line contains an integer , the total number of clouds. The second line contains  space-separated binary integers describing clouds  where .
+
+// Constraints
+
+// Output Format
+
+// Print the minimum number of jumps needed to win the game.
+
+// Sample Input 0
+
+// 7
+// 0 0 1 0 0 1 0
+// Sample Output 0
+
+// 4
+
+
+
+function jumpingOnClouds($c) {
+    
+    $i = count($c);
+    $x = 0;
+    $jumps = 0;
+    
+    while ($x < $i-1){
+       
+            if( $c[$x + 2] == 0 ){
+                $jumps++;
+                $x = $x + 2;
+            }elseif($c[$x + 1] == 0 ) {
+                $jumps++;
+                $x = $x + 1;
+            }
+        
+    }
+    
+    echo $jumps;
+
+}
+
+$nubes= [0, 0, 1, 0, 0, 1, 0];
+$nubes2= [0, 0, 0, 0, 1, 0];
+
+jumpingOnClouds($nubes);
